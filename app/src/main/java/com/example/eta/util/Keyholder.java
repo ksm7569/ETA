1423023e20c17a3505252f9059d819e577267d90
ohseongmin <snders@naver.com>
2025-05-31 18:07:48 +0900

    Delete app/src/main/java/com/example/eta/util/Keyholder.java

--- app/src/main/java/com/example/eta/util/Keyholder.java
+++
@@ -1,9 +0,0 @@
-package com.example.eta.util;
-
-public class Keyholder {
-    private static final String APP_KEY = "AdV49gu52W3aJfseoLWENaix4SWPuihJ96dQx8TN";
-
-    public static String getAppKey() {
-        return APP_KEY;
-    }
-}
